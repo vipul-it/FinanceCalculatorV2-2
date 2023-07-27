698db7cba5e0e08b333ff25572dc0a9d6a38923a
Vipul <vipulmauryaedu@gmail.com>
2023-07-27 13:05:07 +0530

    added

--- src/screen/ChangeCurrency.js
+++ src/screen/ChangeCurrency.js
@@ -1,5 +1,5 @@
 import {View, Text, TextInput, Image, TouchableOpacity} from 'react-native';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import CustomTopLayout from './common/CustomTopLayout';
 import SubHeading from './common/SubHeading';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
@@ -43,34 +43,41 @@ const ChangeCurrency = () => {
         <View className="mt-[4px] mb-2 flex-row mx-5 items-center justify-between">
           <View className="w-[35%] flex justify-center">
             <TouchableOpacity>
-              <View className="flex-row justify-center items-center text-whiteC text-center py-2 px-3  rounded-md bg-primaryDark">
-                {showImage1 ? (
+              {showImage1 ? (
+                <View className="flex-row justify-center items-center text-whiteC text-center py-2 px-3  rounded-md bg-primaryDark">
                   <Image source={allImages.IndianFlag} className="mr-2" />
-                ) : (
+                  <Text className="text-whiteC ">INR</Text>
+                </View>
+              ) : (
+                <View className="flex-row justify-center items-center text-whiteC text-center py-2 px-3  rounded-md bg-primaryDark">
                   <Image source={allImages.UsaFlag} className="mr-2" />
-                )}
-
-                <Text className="text-whiteC ">INR</Text>
-              </View>
+                  <Text className="text-whiteC ">USD</Text>
+                </View>
+              )}
             </TouchableOpacity>
           </View>
           <View className="w-[10%] flex justify-center">
-          <TouchableOpacity onPress={swapImages}><Image
-              className=" w-6 h-6 py-2 px-2 "
-              source={allImages.ConverterArrow}
-            /></TouchableOpacity>
-            
+            <TouchableOpacity onPress={swapImages}>
+              <Image
+                className=" w-6 h-6 py-2 px-2 "
+                source={allImages.ConverterArrow}
+              />
+            </TouchableOpacity>
           </View>
           <View className="w-[35%] flex justify-center">
             <TouchableOpacity>
+              
+                {showImage2 ? (
+                <View className="flex-row justify-center items-center text-whiteC text-center py-2 px-3  rounded-md bg-primaryDark">
+                <Image source={allImages.IndianFlag} className="mr-2" />
+                <Text className="text-whiteC ">INR</Text>
+              </View>
+            ) : (
               <View className="flex-row justify-center items-center text-whiteC text-center py-2 px-3  rounded-md bg-primaryDark">
-              {showImage2 ? (
-                  <Image source={allImages.IndianFlag} className="mr-2" />
-                ) : (
-                  <Image source={allImages.UsaFlag} className="mr-2" />
-                )}
+                <Image source={allImages.UsaFlag} className="mr-2" />
                 <Text className="text-whiteC ">USD</Text>
               </View>
+            )}
             </TouchableOpacity>
           </View>
         </View>
